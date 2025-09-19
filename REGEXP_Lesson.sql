0af5553d8a4de3569ba852e17b3f4ff23f8f15cf
221evans <jayevo95@gmail.com>
2025-09-19 10:29:49 +0100

    Initial commit

---
+++ REGEXP_Lesson.sql
@@ -0,0 +1,23 @@
+
+-- REGEXP Lesson
+
+-- Select all customers whose first name contains either 'elka' or 'ambur'
+SELECT *
+FROM sql_store.customers
+WHERE first_name REGEXP 'elka|ambur';
+-- Select all customers whose last name ends with 'ey' or 'on'
+SELECT *
+FROM sql_store.customers
+WHERE last_name REGEXP 'ey$|on$';
+-- Select all customers whose last name starts with 'my' or 'se'
+SELECT *
+FROM sql_store.customers
+WHERE last_name REGEXP '^my|se';
+-- Select all customers whose last name contains either 'br' or 'u'
+SELECT *
+FROM sql_store.customers
+WHERE last_name REGEXP '[b]r|u';
+
+
+
+
